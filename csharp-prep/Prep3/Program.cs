3d93646af3c1219534c1f4dab97985173712abe4
thebanimal <bkreed597@gmail.com>
2024-03-02 17:14:06 -0700

    Completed Perp3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1,100);
+
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guessString = Console.ReadLine();
+            guess = int.Parse(guessString);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magicNumber);
     }
 }

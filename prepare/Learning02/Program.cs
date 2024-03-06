ec3e727c6d694db9aea757f91ade91c320b1bfab
thebanimal <bkreed597@gmail.com>
2024-03-06 12:09:44 -0700

    Completed Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Front-End Developer";
+        job2._company = "Apple";
+        job2._startYear = 2018;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Bryant Reed";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }

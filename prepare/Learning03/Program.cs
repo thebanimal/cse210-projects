72c8167410969a37a721c5fbd047d6e0cab8e429
thebanimal <bkreed597@gmail.com>
2024-03-13 14:36:37 -0600

    Finish Encapsulation Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction test1 = new Fraction();
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+
+        Fraction test2 = new Fraction(5);
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+
+        Fraction test3 = new Fraction();
+        test3.SetTop(3);
+        test3.SetBottom(4);
+        Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test3.GetDecimalValue());
+
+        Fraction test4 = new Fraction(1,3);
+        Console.WriteLine(test4.GetFractionString());
+        Console.WriteLine(test4.GetDecimalValue());
     }
 }

02798186fdf21fd999c11aa015a00c136c073b6f
thebanimal <bkreed597@gmail.com>
2024-03-14 12:50:41 -0600

    Created Reference class for Encapsulation project

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,53 @@
+using System.Data;
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = 0;
+    }
+    public Reference(string book, int chapter, int verse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+
+    public void SetBook(string book)
+    {
+        _book = book;
+    }
+    public void SetChapter(int chapter)
+    {
+        _chapter = chapter;
+    }
+    public void SetVerse(int verse)
+    {
+        _verse = verse;
+    }
+    public void SetEndVerse(int endVerse)
+    {
+        _endVerse = endVerse;
+    }
+    public string GetDisplayText()
+    {
+        if (_endVerse == 0)
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse} - {_endVerse}";
+        }
+
+    }
+}
